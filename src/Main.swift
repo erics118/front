cf3d3ca4e47f51ee21cacfff32808b92019dcd76
erics118 <52634785+erics118@users.noreply.github.com>
2023-06-27 20:42:00 -0400

    refactor to remove duplicated logic

--- src/Main.swift
+++ src/Main.swift
@@ -1,80 +1,51 @@
 import AppKit
 
-enum E: Error {
-    case FailedToLaunchApp(String)
-    case CouldNotFindApp(String)
-    case NoCLIArg
-    case CouldNotGetFrontmostApp
+enum FrontError: Error {
+    case missingCliArg
+    case missingFrontmostApp
+    case missingApp(String)
+    case failedToLaunchApp(String)
 }
 
-func getFrontApp() throws -> NSRunningApplication {
-    if let app = NSWorkspace.shared.frontmostApplication {
-        return app
-    } else {
-        throw E.CouldNotGetFrontmostApp
-    }
-}
-
-func getHomeDirectory() -> URL {
-    return FileManager.default.homeDirectoryForCurrentUser
-}
-
-func activateAppWithPath(_ path: String) async throws {
-    do {
-        try await NSWorkspace.shared.openApplication(
-            at: URL(fileURLWithPath: path),
-            configuration: NSWorkspace.OpenConfiguration()
-        )
-    } catch {
-        throw E.FailedToLaunchApp(path)
-    }
-}
-
-func activateAppWithName(_ name: String) async throws {
+func appNameToURL(_ name: String) throws -> URL {
     let appPaths = [
         "/Applications/\(name).app",
-        "\(getHomeDirectory())/Applications/\(name).app",
+        "\(FileManager.default.homeDirectoryForCurrentUser)/Applications/\(name).app",
         "/System/Applications/\(name).app",
     ]
 
     for path in appPaths {
         if FileManager.default.fileExists(atPath: path) {
-            try await activateAppWithPath(path)
-            return
+            return URL(fileURLWithPath: path)
         }
     }
 
-    throw E.CouldNotFindApp(name)
+    throw FrontError.missingApp(name)
 }
 
-func activateOrHideAppWithName(_ name: String) async throws {
-    let front = try getFrontApp()
-
-    var front_name = front.localizedName
-
-    // visual studio code is weird. in Finder, it's called "Visual Studio Code",
-    // but in the menu bar it's called "Code"
-    if front_name == "Code" {
-        front_name = "Visual Studio Code"
+func activateApp(_ path: URL) async throws {
+    do {
+        try await NSWorkspace.shared.openApplication(
+            at: path,
+            configuration: NSWorkspace.OpenConfiguration()
+        )
+    } catch {
+        throw FrontError.failedToLaunchApp(path.absoluteString)
     }
+}
 
+func activateOrHideApp(_ path: URL) async throws {
     // TODO: if there are no Finder windows, open a new window for finder
     // instead of trying to hide Finder, which switches to a different app
 
-    if front_name == name {
-        front.hide()
-    } else {
-        try await activateAppWithName(name)
+    guard let front = NSWorkspace.shared.frontmostApplication else {
+        throw FrontError.missingFrontmostApp
     }
-}
 
-func activateOrHideAppWithPath(_ path: String) async throws {
-    let front = try getFrontApp()
-
-    if front.bundleURL?.path == path {
+    if front.bundleURL == path {
         front.hide()
     } else {
-        try await activateAppWithPath(path)
+        try await activateApp(path)
     }
 }
 
@@ -82,14 +53,14 @@ func getFirstArg() throws -> String {
     if let firstArg = CommandLine.arguments.dropFirst().first {
         return firstArg
     } else {
-        throw E.NoCLIArg
+        throw FrontError.missingCliArg
     }
 }
 
 let arg = try getFirstArg()
 
 if arg.starts(with: "/") {
-    try await activateOrHideAppWithPath(arg)
+    try await activateOrHideApp(URL(fileURLWithPath: arg))
 } else {
-    try await activateOrHideAppWithName(arg)
+    try await activateOrHideApp(appNameToURL(arg))
 }
